8d10451c6af90fd2451186c1498300e1c74bc80f
renanns <atomsfrontenf@gmail.com>
2024-05-24 13:59:18 -0300

    fix: correting test file to render tests

--- src/styles/render-theme.js
+++
@@ -1,7 +0,0 @@
-import theme from './theme';
-import { ThemeProvider } from 'styled-components';
-
-const renderTheme = ( {children} ) => {
-    return <ThemeProvider theme={theme}>{children}</ThemeProvider>
-};
-export default renderTheme;
--- src/templates/app/App.test.jsx
+++ src/templates/app/App.test.jsx
@@ -1,11 +1,13 @@
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/dom';
+import { render } from '@testing-library/react';
 
-import renderTheme from '../../styles/render-theme';
 import theme from '../../styles/theme';
+import { ThemeProvider } from 'styled-components';
 
 import Home from '.';
+
 test('renders learn react link', () => {
-  render(renderTheme(<Home />));
+  render(<ThemeProvider theme={theme}> <Home /> </ThemeProvider>);
   const heading = screen.getByRole('heading', {name: 'Hello'});
 
   expect(heading.parentElement).toHaveStyle({
@@ -13,5 +15,5 @@ test('renders learn react link', () => {
   });
   
   expect(heading.parentElement).toMatchSnapshot();
-  expect(heading.parentElement).toHaveStyleRule('background', 'red');
+  expect(heading.parentElement).toHaveStyleRule('background', 'blue');
 });
